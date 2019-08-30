9326b3d27668581caa5199ba6e8664c48c69cac3
Lucas Goes <lucasgoes@BUT-FEI57.grupocarrera.local>
2019-08-30 15:46:26 -0300

    adicionando aula11

---
+++ aula11/exe008.js
@@ -0,0 +1,8 @@
+var vel = 78.2
+console.log(`A velocidade do seu carro é ${vel}Km`)
+
+if (vel > 60) { //condição simples!
+    console.log(`Você ultrapassou o limite de velocidade. MULTADO!`)
+}
+
+console.log(`Dirija sempre usando cinto de segurança`)
